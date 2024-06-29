9b6cfe3a2466b1fbc7ac338747880d0a8635872c
harshadj970 <harshadj970@gmail.com>
2024-06-29 11:29:15 +0530

    add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-clone-io.vercel.app",
   })
 );
 app.use("/uploads", express.static(__dirname + "/uploads"));
